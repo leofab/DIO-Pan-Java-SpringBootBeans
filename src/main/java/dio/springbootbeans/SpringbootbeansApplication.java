2e965b2d361f2339fa2bf8f94158736d7759ec62
leofab <leofabr3@gmail.com>
2023-03-15 11:48:14 -0300

    first commit

---
+++ src/main/java/dio/springbootbeans/SpringbootbeansApplication.java
@@ -0,0 +1,13 @@
+package dio.springbootbeans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootbeansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootbeansApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dio/springbootbeans/SpringbootbeansApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.springbootbeans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootbeansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
