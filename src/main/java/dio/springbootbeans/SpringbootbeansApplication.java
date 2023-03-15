93078f5bc7d580d9dfb093025e3d3abb8dc4de08
leofab <leofabr3@gmail.com>
2023-03-15 11:52:59 -0300

    SpringbootBeansApplication Beans and conversor component implement

--- src/main/java/dio/springbootbeans/SpringbootbeansApplication.java
+++ src/main/java/dio/springbootbeans/SpringbootbeansApplication.java
@@ -1,13 +1,26 @@
 package dio.springbootbeans;
 
+import com.google.gson.Gson;
+import dio.springbootbeans.app.ConversorJson;
+import dio.springbootbeans.app.ViaCepResponse;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringbootbeansApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootbeansApplication.class, args);
+	}
+	@Bean
+	public CommandLineRunner run(ConversorJson conversor) throws Exception {
+		return args -> {
+			String json = "{\"cep\": \"01001-000\",\"logradouro\": \"Praça da Sé\",\"localidade\": \"São Paulo\"}";
+			ViaCepResponse response = conversor.converter(json);
+			System.out.println("Dados do CEP: " + response);
+		};
 	}
 
 }
